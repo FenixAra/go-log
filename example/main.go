edf6c8aeb95bd694e474a5c4f86bbba3c01ca25c
Aravindhan <aravindhan.a@lynk.co.in>
2019-11-28 16:27:54 +0530

    :white_check_mark: Fixed example files

--- example/main.go
+++ example/main.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	config := log.NewConfig("Test_App")
+	config.SetFilePathSizeEnum(log.FULL)
 	l := log.New(config)
 
 	l.Fatalf("This is for testing. Str: %s, Int: %d, Err: %+v", "Hello", 100, errors.New("No rows"))
+	l.Infof("This is for testing. Str: %s, Int: %d, Err: %+v", "Hello", 100, errors.New("No rows"))
+	l.Debugf("This is for testing. Str: %s, Int: %d, Err: %+v", "Hello", 100, errors.New("No rows"))
 }
